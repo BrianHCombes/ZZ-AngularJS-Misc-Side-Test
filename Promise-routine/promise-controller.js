9d865811e2f6b77bb79c4931f8851ff920dc1d58
BrianHCombes <briancombes59@yahoo.com>
2021-06-27 00:37:40 -0600

    Side test for modules id done. This commit is for progress getting the promise testing in place.

--- Promise-routine/promise-controller.js
+++ Promise-routine/promise-controller.js
@@ -1,11 +1,19 @@
  angular.module('PromiseRoutine', [])
         
-            .controller('promiseCtrl', ['$log', '$scope', function($log, $scope) {
+            .controller('promiseCtrl', ['$log', '$scope',  'PromDat', function($log, $scope, PromDat) {
                
                 var prom = this;
                 prom.promVar1 = "YES! This is from the promiseCtrl Controller";
+                
+                var promiseData = function(promData){
+                  
+                  console.log("Promise data returned to the controller is: " + JSON.stringify(promData));
+                }
+                
+                PromDat.getPromiseData(promiseData);
             
-            }]);
+            }])
+          
                 
             
  
--- Promise-routine/promise-http-service.js
+++ Promise-routine/promise-http-service.js
@@ -1,7 +1,25 @@
-angular.module("viewNav")
-  .factory('VnData', ['$http', function($http){
+angular.module("PromiseRoutine")
+  .factory('PromDat', ['$http', function($http){
+  //.factory('PromDat', [function(){
    
     return  {
+      
+      getPromiseData:       function(promiseDataRef){
+                            
+                              $http.get('promise-routine.json', { cache: true }).then(function(resp) {
+                              //$http.get('https://www.tarptiedown.com/data_common/json-client-css.json', { cache: true }).then(function(resp) {    
+                                        console.log("Data is: " + JSON.stringify(resp.data));
+                                        console.log("Data is: " + resp.data.name);
+                                        return promiseDataRef(resp.data);
+                              },
+                              function (resp){
+                                console.error("ERROR! ERROR! is: " + JSON.stringify(resp));
+                              });
+                            },
+                          
+      test:               function(){
+                              return "At the HTTP service";
+                          }
       
     }
   }])
