75d8b7a2ae754d2b0f9d5c11f4cabb6327f02804
BrianHCombes <briancombes59@yahoo.com>
2021-06-30 00:19:24 -0600

    Continuing with more promise architecture (and now) playing with async/await protocol. That's where this has to go to stay current.

--- Promise-routine/promise-controller.js
+++ Promise-routine/promise-controller.js
@@ -11,8 +11,27 @@
                   console.log("Promise data returned to the controller is: " + JSON.stringify(promData));
                   prom.promiseData1 = "Promise data returned to the controller is: " + JSON.stringify(promData);
                 }
-                
                 PromDat.getPromiseData2(promiseData);
+                
+                
+                // Playing around with async / await per https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
+                var hello = async function() { return "Hello" };
+               
+                console.log( hello());
+                hello().then(console.log);
+                hello().then((value) => console.log(value));
+                
+                
+                
+                
+                var hello2 = async function() {
+                  var greeting = await Promise.resolve("Hello2");
+                  return greeting;
+                };
+
+                hello2().then(alert);
+                
+                
             }])
           
                 
